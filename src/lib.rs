89af7b108bd63598d8bd2e2d964f601571bf6a6b
eric tu <tu.eric@hotmail.com>
2024-02-28 16:49:23 +0700

    add default build target to config.toml

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,6 @@ use wasm_bindgen::prelude::*;
 use web_sys::console;
 use zcash_note_encryption::{batch, try_compact_note_decryption, try_note_decryption};
 
-
 #[cfg(feature = "parallel")]
 pub use wasm_bindgen_rayon::init_thread_pool;
 
