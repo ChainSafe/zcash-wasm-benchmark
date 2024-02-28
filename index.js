d77e2112cfda0b85e3fb5faa7377ef830d389d73
eric tu <tu.eric@hotmail.com>
2024-02-28 17:15:05 +0700

    easier to run tests in browser

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-/*
- * Copyright 2022 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import init, { initThreadPool, what, proof } from './pkg/zcash_wasm_benchmark.js';
-
-await init();
 let num_concurrency = navigator.hardwareConcurrency;
 console.log("num_concurrency: ", num_concurrency);
-await initThreadPool(num_concurrency);
-// // 1...10
-// let arr = Int32Array.from({ length: 10 }, (_, i) => i + 1);
-// if (sum(arr) !== 55) {
-//   throw new Error('Wrong result.');
-// }
-proof();
-// Note: this will be overridden by the Playwright test runner.
-// The default implementation is provided only for manual testing.
-globalThis.onDone ??= () => console.log('OK');
 
-onDone();
+function setupBtn(id, { proof }) {
+    // Assign onclick handler + enable the button.
+    Object.assign(document.getElementById(id), {
+      async onclick() {
+        const start = performance.now();
+        proof()
+        const time = performance.now() - start;
+  
+        console.log(`${time.toFixed(2)} ms`);
+      },
+      disabled: false
+    });
+  }
+
+(async function initSingleThread() {
+    const singleThread = await import('./pkg/serial/zcash_wasm_benchmark.js');
+    await singleThread.default();
+    setupBtn('singleThread', singleThread);
+  })();
+  
+(async function initMultiThread() {
+    const multiThread = await import('./pkg/parallel/zcash_wasm_benchmark.js');
+    await multiThread.default();
+
+    await multiThread.initThreadPool(num_concurrency);
+    setupBtn('multiThread', multiThread);
+})();
