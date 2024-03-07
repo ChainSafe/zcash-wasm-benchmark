341028b58216441e48574941de860692791814f3
eric tu <tu.eric@hotmail.com>
2024-03-06 20:45:33 -0500

    remove some comments

--- src/types.rs
+++ src/types.rs
@@ -214,24 +214,3 @@ impl TryFrom<&CompactSaplingOutput> for sapling::note_encryption::CompactOutputD
         })
     }
 }
-
-// #[wasm_bindgen]
-// pub fn s(x: Vec<CompactOrchardAction>) -> u8 {
-//     11
-// }
-// #[wasm_bindgen]
-// pub struct WasmUint8Array(Vec<u8>);
-
-// #[wasm_bindgen]
-// impl WasmUint8Array {
-//     #[wasm_bindgen(constructor)]
-//     pub fn new(size: usize) -> Self {
-//         let buffer = vec![0; size];
-//         Self { 0: buffer }
-//     }
-
-//     #[wasm_bindgen(getter)]
-//     pub fn view(&mut self) -> js_sys::Uint8Array {
-//         unsafe { js_sys::Uint8Array::view_mut_raw(self.0.as_mut_ptr(), self.0.len()) }
-//     }
-// }
