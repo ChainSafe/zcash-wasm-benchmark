773cdb082bc4adbdefd8353e095bc6fb5f524d80
eric tu <tu.eric@hotmail.com>
2024-03-07 12:49:46 -0500

    split into modules

--- src/commitment_tree.rs
+++ src/commitment_tree.rs
@@ -3,14 +3,14 @@
  */
 use std::convert::TryInto;
 
-use zcash_primitives::consensus::BlockHeight;
+use incrementalmerkletree::{Position, Retention};
+use orchard::note::ExtractedNoteCommitment;
+use orchard::tree::MerkleHashOrchard;
 use shardtree::store::memory::MemoryShardStore;
 use shardtree::ShardTree;
-use incrementalmerkletree::{Position, Retention};
-use orchard::tree::MerkleHashOrchard;
-use orchard::note::ExtractedNoteCommitment;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
+use zcash_primitives::consensus::BlockHeight;
 
 use crate::types::CompactTx;
 
@@ -18,7 +18,8 @@ pub const ORCHARD_SHARD_HEIGHT: u8 = { orchard::NOTE_COMMITMENT_TREE_DEPTH as u8
 
 type OrchardMemoryShardStore = MemoryShardStore<orchard::tree::MerkleHashOrchard, BlockHeight>;
 
-pub type OrchardCommitmentTree = ShardTree<OrchardMemoryShardStore, { ORCHARD_SHARD_HEIGHT * 2 }, ORCHARD_SHARD_HEIGHT>;
+pub type OrchardCommitmentTree =
+    ShardTree<OrchardMemoryShardStore, { ORCHARD_SHARD_HEIGHT * 2 }, ORCHARD_SHARD_HEIGHT>;
 
 // max number of checkpoints our tree impl can cache to jump back to
 const MAX_CHECKPOINTS: usize = 1;
@@ -26,14 +27,13 @@ const MAX_CHECKPOINTS: usize = 1;
 // insert n_nodes integers into a new tree and benchmark it
 #[wasm_bindgen]
 pub fn batch_insert_mock_data(n_nodes: usize, n_genwitness: usize) {
+    let mut b = [0_u8; 32];
 
-    let commitments = (0..n_nodes)
-        .map(|i| {
-            let mut b = [0_u8; 32];
-            b[..4].copy_from_slice(&i.to_be_bytes());
-            MerkleHashOrchard::from_bytes(&b).unwrap()
-        });
-        
+    let commitments = (0..n_nodes).map(move |i| {
+        b[..4].copy_from_slice(&i.to_be_bytes());
+        MerkleHashOrchard::from_bytes(&b).unwrap()
+    });
+
     benchmark_tree(commitments, n_genwitness);
 }
 
@@ -41,16 +41,16 @@ pub fn batch_insert_mock_data(n_nodes: usize, n_genwitness: usize) {
 #[wasm_bindgen]
 pub fn batch_insert_txn_notes(vtxs: Box<[CompactTx]>, n_genwitness: usize) {
     let commitments = vtxs // create an iterator over the commitments to add
-        .into_iter()
-        .map(|tx| {
+        .iter()
+        .flat_map(|tx| {
             tx.actions
                 .iter()
                 .map(|action| {
-                    ExtractedNoteCommitment::from_bytes(action.cmx.as_ref().try_into().unwrap()).unwrap()
+                    ExtractedNoteCommitment::from_bytes(action.cmx.as_ref().try_into().unwrap())
+                        .unwrap()
                 })
                 .collect::<Vec<_>>()
         })
-        .flatten()
         .map(|cmx| MerkleHashOrchard::from_cmx(&cmx));
 
     benchmark_tree(commitments, n_genwitness);
@@ -60,31 +60,49 @@ pub fn batch_insert_txn_notes(vtxs: Box<[CompactTx]>, n_genwitness: usize) {
 /// The benchmarks marks the first 10 elements as ones we are interested in maintaining the witnesses for
 /// and then adds the remainder as ephemeral nodes (ones that will be pruned and just serve to update the witness)
 /// n_genwitness is the number of nodes to mark as needing a witness generated for them
-fn benchmark_tree(commitments: impl Iterator<Item = MerkleHashOrchard> + Clone, n_genwitness: usize) {
+fn benchmark_tree(
+    commitments: impl Iterator<Item = MerkleHashOrchard> + Clone,
+    n_genwitness: usize,
+) {
     let mut tree = OrchardCommitmentTree::new(OrchardMemoryShardStore::empty(), MAX_CHECKPOINTS);
-    
+
     // checkpoint the tree at the start so it actually builds witnesses as we go
-    // and prunes out ephemeral nodes. Otherwise it will just store all ephemeral nodes and 
+    // and prunes out ephemeral nodes. Otherwise it will just store all ephemeral nodes and
     // to hash later which uses more memory and isn't a useful benchmark
     let _success = tree.checkpoint(0.into()).unwrap();
 
-    console::log_1(&format!("Adding {} commitments to tree", commitments.clone().count()).as_str().into());
-    console::log_1(&format!("Maintaining witness for {} leaves", n_genwitness).as_str().into());
+    console::log_1(
+        &format!("Adding {} commitments to tree", commitments.clone().count())
+            .as_str()
+            .into(),
+    );
+    console::log_1(
+        &format!("Maintaining witness for {} leaves", n_genwitness)
+            .as_str()
+            .into(),
+    );
 
-    // mark the first n_genwitness, the rest as ephemeral 
-    let ours = commitments.clone().take(n_genwitness).map(|cmx| (cmx, Retention::Marked));
-    let rest = commitments.skip(n_genwitness).map(|cmx| (cmx, Retention::<BlockHeight>::Ephemeral));
+    // mark the first n_genwitness, the rest as ephemeral
+    let ours = commitments
+        .clone()
+        .take(n_genwitness)
+        .map(|cmx| (cmx, Retention::Marked));
+    let rest = commitments
+        .skip(n_genwitness)
+        .map(|cmx| (cmx, Retention::<BlockHeight>::Ephemeral));
 
     console::time_with_label("Adding our notes to tree");
     let (last_added, _incomplete) = tree.batch_insert(Position::from(0), ours).unwrap().unwrap();
     console::time_end_with_label("Adding our notes to tree");
 
     console::time_with_label("Updating witnesses with rest");
-    let (_, _incomplete) = tree.batch_insert(last_added+1, rest).unwrap().unwrap();
+    let (_, _incomplete) = tree.batch_insert(last_added + 1, rest).unwrap().unwrap();
     console::time_end_with_label("Updating witnesses with rest");
 
     console::time_with_label("Calculating witness for first added");
-    let witness = tree.witness_at_checkpoint_depth(Position::from(0), 0).unwrap();
+    let witness = tree
+        .witness_at_checkpoint_depth(Position::from(0), 0)
+        .unwrap();
     console::time_end_with_label("Calculating witness for first added");
 
     console::log_1(&format!("Witness {:?}", witness).as_str().into());
---
+++ src/trial_decryption.rs
@@ -0,0 +1,201 @@
+use orchard::{
+    keys::{FullViewingKey, PreparedIncomingViewingKey, Scope, SpendingKey},
+    note_encryption::{CompactAction, OrchardDomain},
+};
+use rand::rngs::OsRng;
+
+use std::convert::TryInto;
+
+use crate::types::*;
+use ff::Field;
+use rayon::prelude::*;
+use sapling::{
+    keys::SaplingIvk,
+    note_encryption::{CompactOutputDescription, SaplingDomain, Zip212Enforcement},
+};
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+use zcash_note_encryption::{batch, BatchDomain, Domain, ShieldedOutput, COMPACT_NOTE_SIZE};
+
+#[wasm_bindgen]
+pub fn decrypt_vtx_orchard(vtxs: Box<[CompactTx]>) -> u32 {
+    console::time_with_label("Converting VTX");
+    let compact = vtxs
+        .iter()
+        .flat_map(|tx| {
+            tx.actions
+                .iter()
+                .map(|action| {
+                    let action: CompactAction = action.try_into().unwrap();
+                    let domain = OrchardDomain::for_nullifier(action.nullifier());
+                    (domain, action)
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    console::time_end_with_label("Converting VTX");
+    let ivks = dummy_ivk_orchard(1);
+
+    console::log_1(
+        &format!(
+            "Attempting to batch decrypt {} Orchard txns for {} Viewing keys",
+            compact.len(),
+            ivks.len()
+        )
+        .into(),
+    );
+    decrypt_compact(&ivks, &compact)
+}
+
+#[wasm_bindgen]
+pub fn decrypt_vtx_sapling(vtxs: Box<[CompactTx]>) -> u32 {
+    console::time_with_label("Converting VTX");
+    let compact = vtxs
+        .iter()
+        .flat_map(|tx| {
+            tx.outputs
+                .iter()
+                .map(|output| {
+                    let output: CompactOutputDescription = output.try_into().unwrap();
+                    (SaplingDomain::new(Zip212Enforcement::Off), output)
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Box<[_]>>();
+    console::time_end_with_label("Converting VTX");
+
+    let ivks = dummy_ivk_sapling(1);
+
+    console::log_1(
+        &format!(
+            "Attempting to batch decrypt Sapling {} txns for {} Viewing keys",
+            compact.len(),
+            ivks.len()
+        )
+        .into(),
+    );
+
+    decrypt_compact(ivks.as_slice(), &compact)
+}
+
+#[wasm_bindgen]
+pub fn decrypt_vtx_both(vtxs: Box<[CompactTx]>) -> u32 {
+    console::time_with_label("Converting VTX");
+    let (actions, outputs) =
+        vtxs.iter()
+            .fold((vec![], vec![]), |(mut actions, mut outputs), tx| {
+                let mut act = tx
+                    .actions
+                    .iter()
+                    .map(|action| {
+                        let action: CompactAction = action.try_into().unwrap();
+                        let domain = OrchardDomain::for_nullifier(action.nullifier());
+                        (domain, action)
+                    })
+                    .collect::<Vec<_>>();
+                let mut opt = tx
+                    .outputs
+                    .iter()
+                    .map(|output| {
+                        let output: CompactOutputDescription = output.try_into().unwrap();
+                        (SaplingDomain::new(Zip212Enforcement::Off), output)
+                    })
+                    .collect::<Vec<_>>();
+                actions.append(&mut act);
+                outputs.append(&mut opt);
+                (actions, outputs)
+            });
+    drop(vtxs);
+    console::time_end_with_label("Converting VTX");
+
+    let ivks = dummy_ivk_sapling(1);
+
+    console::log_1(
+        &format!(
+            "Attempting to batch decrypt Sapling {} txns for {} Viewing keys",
+            outputs.len(),
+            ivks.len()
+        )
+        .into(),
+    );
+
+    let s = decrypt_compact(ivks.as_slice(), &outputs);
+    drop(outputs);
+
+    let ivks = dummy_ivk_orchard(1);
+
+    console::log_1(
+        &format!(
+            "Attempting to batch decrypt {} Orchard txns for {} Viewing keys",
+            actions.len(),
+            ivks.len()
+        )
+        .into(),
+    );
+    let o = decrypt_compact(&ivks, &actions);
+
+    s + o
+}
+
+fn decrypt_compact<D: BatchDomain, Output: ShieldedOutput<D, COMPACT_NOTE_SIZE>>(
+    ivks: &[D::IncomingViewingKey],
+    compact: &[(D, Output)],
+) -> u32
+where
+    (D, Output): Sync + Send,
+    <D as Domain>::Note: Send + std::fmt::Debug,
+    <D as Domain>::Recipient: Send + std::fmt::Debug,
+    <D as Domain>::IncomingViewingKey: Sync + std::fmt::Debug,
+{
+    let num_parallel = rayon::current_num_threads();
+    console::log_1(&format!("Rayon available parallelism Num Parallel: {}", num_parallel).into());
+
+    console::time_with_label(&format!("Decrypted Total {} transactions", compact.len()));
+    let results = compact
+        .par_chunks(usize::div_ceil(compact.len(), num_parallel))
+        .enumerate()
+        .map(|(i, c)| {
+            console::time_with_label(&format!(
+                "Decrypted chunk {} of {} transactions",
+                i,
+                c.len()
+            ));
+            let r = batch::try_compact_note_decryption(ivks, c);
+            console::time_end_with_label(&format!(
+                "Decrypted chunk {} of {} transactions",
+                i,
+                c.len()
+            ));
+            r
+        })
+        .flatten()
+        .collect::<Vec<_>>();
+
+    console::time_end_with_label(&format!("Decrypted Total {} transactions", compact.len()));
+
+    let valid_results = results.into_iter().flatten().collect::<Vec<_>>();
+    if valid_results.is_empty() {
+        console::log_1(&"No notes for this address".to_string().into());
+    } else {
+        console::log_1(&format!("Notes: {:?}", valid_results).into());
+    }
+    compact.len() as u32
+}
+
+fn dummy_ivk_sapling(count: usize) -> Vec<sapling::note_encryption::PreparedIncomingViewingKey> {
+    let mut rng = OsRng;
+
+    (1..=count)
+        .map(|_| SaplingIvk(jubjub::Fr::random(&mut rng)))
+        .map(|k| sapling::note_encryption::PreparedIncomingViewingKey::new(&k))
+        .collect::<Vec<_>>()
+}
+
+fn dummy_ivk_orchard(count: usize) -> Vec<PreparedIncomingViewingKey> {
+    (1..=count)
+        .map(|i| {
+            let fvk = FullViewingKey::from(&SpendingKey::from_bytes([i as u8; 32]).unwrap());
+            PreparedIncomingViewingKey::new(&fvk.to_ivk(Scope::External))
+        })
+        .collect::<Vec<_>>()
+}
