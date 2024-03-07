85e8c3fe20b6c17c26197a48e139ed70c8ea6216
Willem Olding <willemolding@gmail.com>
2024-03-08 10:26:37 +1100

    adds parallel updates to commitment tree using rayon

--- src/commitment_tree.rs
+++ src/commitment_tree.rs
@@ -6,13 +6,13 @@ use std::convert::TryInto;
 use incrementalmerkletree::{Position, Retention};
 use orchard::note::ExtractedNoteCommitment;
 use orchard::tree::MerkleHashOrchard;
+use rayon::prelude::*;
 use shardtree::store::memory::MemoryShardStore;
 use shardtree::ShardTree;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-use zcash_primitives::consensus::BlockHeight;
-use rayon::prelude::*;
 use zcash_client_backend::data_api::SAPLING_SHARD_HEIGHT;
+use zcash_primitives::consensus::BlockHeight;
 
 use crate::types::CompactTx;
 
@@ -31,10 +31,12 @@ const MAX_CHECKPOINTS: usize = 1;
 pub fn batch_insert_mock_data(n_nodes: usize, n_genwitness: usize) {
     let mut b = [0_u8; 32];
 
-    let commitments = (0..n_nodes).map(move |i| {
-        b[..4].copy_from_slice(&i.to_be_bytes());
-        MerkleHashOrchard::from_bytes(&b).unwrap()
-    }).collect::<Vec<_>>();
+    let commitments = (0..n_nodes)
+        .map(move |i| {
+            b[..4].copy_from_slice(&i.to_be_bytes());
+            MerkleHashOrchard::from_bytes(&b).unwrap()
+        })
+        .collect::<Vec<_>>();
 
     benchmark_tree(&commitments, n_genwitness);
 }
@@ -53,7 +55,8 @@ pub fn batch_insert_txn_notes(vtxs: Box<[CompactTx]>, n_genwitness: usize) {
                 })
                 .collect::<Vec<_>>()
         })
-        .map(|cmx| MerkleHashOrchard::from_cmx(&cmx)).collect::<Vec<_>>();
+        .map(|cmx| MerkleHashOrchard::from_cmx(&cmx))
+        .collect::<Vec<_>>();
 
     benchmark_tree(&commitments, n_genwitness);
 }
@@ -62,10 +65,7 @@ pub fn batch_insert_txn_notes(vtxs: Box<[CompactTx]>, n_genwitness: usize) {
 /// The benchmarks marks the first 10 elements as ones we are interested in maintaining the witnesses for
 /// and then adds the remainder as ephemeral nodes (ones that will be pruned and just serve to update the witness)
 /// n_genwitness is the number of nodes to mark as needing a witness generated for them
-fn benchmark_tree(
-    commitments: &[MerkleHashOrchard],
-    n_genwitness: usize,
-) {
+fn benchmark_tree(commitments: &[MerkleHashOrchard], n_genwitness: usize) {
     let mut tree = OrchardCommitmentTree::new(OrchardMemoryShardStore::empty(), MAX_CHECKPOINTS);
 
     // checkpoint the tree at the start so it actually builds witnesses as we go
@@ -90,16 +90,12 @@ fn benchmark_tree(
         .clone()
         .take(n_genwitness)
         .map(|cmx| (*cmx, Retention::Marked));
-    // let rest = commitments
-    //     .skip(n_genwitness)
-    //     .map(|cmx| (cmx, Retention::<BlockHeight>::Ephemeral));
 
     console::time_with_label("Adding our notes to tree");
     let (last_added, _incomplete) = tree.batch_insert(Position::from(0), ours).unwrap().unwrap();
     console::time_end_with_label("Adding our notes to tree");
 
     console::time_with_label("Updating witnesses with rest");
-    
     // Create subtrees from the note commitments in parallel.
     const CHUNK_SIZE: usize = 1024;
     let start_position = last_added + 1;
@@ -114,14 +110,18 @@ fn benchmark_tree(
             shardtree::LocatedTree::from_iter(
                 start..end,
                 SAPLING_SHARD_HEIGHT.into(),
-                chunk.iter().map(|cmx| (*cmx, Retention::<BlockHeight>::Ephemeral)),
+                chunk
+                    .iter()
+                    .map(|cmx| (*cmx, Retention::<BlockHeight>::Ephemeral)),
             )
         })
         .map(|res| (res.subtree, res.checkpoints))
         .collect::<Vec<_>>();
 
-
-    // let (_, _incomplete) = tree.batch_insert(last_added + 1, rest).unwrap().unwrap();
+    // add the subtrees
+    for (subtree, checkpoints) in subtrees {
+        tree.insert_tree(subtree, checkpoints).unwrap();
+    }
     console::time_end_with_label("Updating witnesses with rest");
 
     console::time_with_label("Calculating witness for first added");
