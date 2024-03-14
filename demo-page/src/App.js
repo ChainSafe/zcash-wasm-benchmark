278d13f23c76e0695c5f774bcbc10f3226050716
Willem Olding <willemolding@gmail.com>
2024-03-14 22:52:08 +1100

    adds basic page which can call wasm successfully

--- demo-page/src/App.js
+++ demo-page/src/App.js
@@ -1,18 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import initWasm, { sapling_decrypt_wasm, initThreadPool } from "../wasm-pkg/parallel";
+
+const SAPLING_ACTIVATION = 419200;
+const ORCHARD_ACTIVATION = 653600;
+
+const MAINNET_LIGHTWALLETD_PROXY = "http://localhost:443";
+const TESTNET_LIGHTWALLETD_PROXY = "http://testnet.localhost:443";
 
 export function App() {
 
-    let [startBlock, setStartBlock] = useState(1);
-    let [endBlock, setEndBlock] = useState(1);
-    let [paymentFrequency, setPaymentFrequency] = useState(1);
+    // Setup
+    useEffect(() => {
+        async function init() {
+            await initWasm();
+            let threads = navigator.hardwareConcurrency || 1;
+            console.log("Initializing thread pool with", threads, "threads");
+            await initThreadPool(threads);
+        }
+        init();
+    }, []);
 
-    function trialDecryption() {
+    // State
+    let [startBlock, setStartBlock] = useState(SAPLING_ACTIVATION);
+    let [endBlock, setEndBlock] = useState(SAPLING_ACTIVATION + 1000);
+    let [network, setNetwork] = useState("mainnet");
+    let [shieldedPool, setShieldedPool] = useState("sapling");
+    let [lightwalletdProxy, setLightwalletdProxy] = useState(MAINNET_LIGHTWALLETD_PROXY);
+    let [paymentFrequency, setPaymentFrequency] = useState(0);
+    let [proofGenerationSpends, setProofGenerationSpends] = useState(1);
+
+    // Event Handlers
+    function onNetworkUpdate(network) {
+        setNetwork(network);
+        if (network === "mainnet") {
+            setLightwalletdProxy(MAINNET_LIGHTWALLETD_PROXY);
+        } else {
+            setLightwalletdProxy(TESTNET_LIGHTWALLETD_PROXY);
+        }
     }
 
-    function treeStateSync() {
+    function onPoolUpdate(pool) {
+        setShieldedPool(pool);
+        if (pool === "sapling") {
+            setStartBlock(SAPLING_ACTIVATION);
+            setEndBlock(SAPLING_ACTIVATION + 1000);
+        } else {
+            setStartBlock(ORCHARD_ACTIVATION);
+            setEndBlock(ORCHARD_ACTIVATION + 1000);
+        }
     }
 
-    function proofGeneration() {
+    async function runTrialDecryption() {
+        await sapling_decrypt_wasm(startBlock, endBlock);
+    }
+
+    async function runTreeStateSync() {
+    }
+
+    async function runProofGeneration() {
 
     }
 
@@ -22,7 +68,26 @@ export function App() {
 
             Open the browser console to see results of benchmarks
 
+            <h2>Global Settings</h2>
             <div>
+                <label>
+                    Network:
+                    <select value={network} onChange={e => onNetworkUpdate(e.target.value)}>
+                        <option value="mainnet">Mainnet</option>
+                        <option value="testnet">Testnet</option>
+                    </select>   
+                </label>
+                <label>
+                    Shielded Pool:
+                    <select value={shieldedPool} onChange={e => onPoolUpdate(e.target.value)}>
+                        <option value="sapling">Sapling</option>
+                        <option value="orchard">Orchard</option>
+                    </select>  
+                </label>
+                <label>
+                    lightwalletd URL (must have grpc-web proxy):
+                    <input type="text" value={lightwalletdProxy} onChange={(e) => setLightwalletdProxy(e.target.value)} />
+                </label>
                 <label>
                     Start Block:
                     <input type="number" value={startBlock} onChange={(e) => setStartBlock(e.target.value)} />
@@ -33,25 +98,39 @@ export function App() {
                 </label>
             </div>
 
+            <hr />
+
             <div>
                 <h2>Trial Decryption</h2>
-                <label>
-                    Payment Frequency:
-                    <input type="number" value={paymentFrequency} onChange={(e) => setPaymentFrequency(e.target.value)} />
-                </label>
-                <button onClick={trialDecryption}>Start</button>
+                <p>
+                    Download all blocks in the provided range and trial decrypt all transactions
+                </p>
+                <button onClick={runTrialDecryption}>Start</button>
             </div>
+
+            <hr />
+
             <div>
                 <h2>Treestate Sync</h2>
-                <button onClick={treeStateSync}>Start</button>
+                <p>Retrieve the commitment tree frontier as of start_block and insert all note commitments to advance the tree up to end_block.</p>
+                <p>To simulate a wallet that is receiving outputs and tracking witnesses this test optionally mark random outputs every X blocks to update a witness for.</p>
+                <label>
+                    Payment Frequency:
+                    <input type="number" value={paymentFrequency} onChange={(e) => setPaymentFrequency(Number(e.target.value))} />
+                </label>
+                <button onClick={runTreeStateSync}>Start</button>
             </div>
+
+            <hr />
+
             <div>
                 <h2>Proof Generation</h2>
+                <p>Generate a proof for a dummy transaction with the given number of spends</p>
                 <label>
                     Number of spends:
-                    <input type="number" defaultValue={1} />
+                    <input type="number" value={proofGenerationSpends} onChange={e => setProofGenerationSpends(Number(e.target.value))} />
                 </label>
-                <button onClick={proofGeneration}>Start</button>
+                <button onClick={runProofGeneration}>Start</button>
             </div>
         </div>
     );
